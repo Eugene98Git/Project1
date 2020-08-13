5a64d39b17ba436999defc92934d8f693641f400
Eugene <evgenya98@gmail.com>
2020-08-13 13:18:02 +0500

    commit_1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-var name = "one";
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const lastFilm1 = prompt('Назовите один из последних просмотренных фильмов.', ''),
+      lastFilmCost1 = prompt('На сколько оцените его?', ''),
+      lastFilm2 = prompt('Назовите один из последних просмотренных фильмов.', ''),
+      lastFilmCost2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies [lastFilm1] = lastFilmCost1;
+personalMovieDB.movies [lastFilm2] = lastFilmCost2;
+
+console.log(personalMovieDB);
